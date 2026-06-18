5222a22ca5cb06dbb741caaf8b3e168aa33a7478
MertAliEsen <81825582+MertAliEsen@users.noreply.github.com>
2026-06-18 16:51:54 +0200

    Add files via upload

---
+++ myFifthApp.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class myFifthApp {
+    public static void printCharacters(char c, int width, int rows) {
+        for (int row = 0; row < rows; row++) {
+            for (int column = 0; column < width; column++) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("enter a character ");
+        char c = input.next().charAt(0);
+
+        System.out.print("enter characters in one row ");
+        int width = input.nextInt();
+
+        System.out.print("enter rows ");
+        int rows = input.nextInt();
+
+        printCharacters(c, width, rows);
+
+        input.close();
+    }
+}
