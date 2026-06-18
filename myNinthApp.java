d14e47dea3b4c9cdc7833cb957b6e01e3947c8f2
MertAliEsen <81825582+MertAliEsen@users.noreply.github.com>
2026-06-18 16:54:57 +0200

    Add files via upload

---
+++ myNinthApp.java
@@ -0,0 +1,13 @@
+public class myNinthApp {
+    public static void main(String[] args) {
+        Warehouse warehouse = new Warehouse(1, 5000, "Tom", "tom@mail.com", "12345");
+
+        warehouse.addGoods(3000);
+        warehouse.removeGoods(1000);
+        warehouse.addGoods(2500);
+        warehouse.checkOccupancy();
+        System.out.println();
+        warehouse.updateContact("tom2@mail.com", "54321");
+        warehouse.addGoods(1000);
+    }
+}
