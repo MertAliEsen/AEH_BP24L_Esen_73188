b3281485de49a3548cf9c0b0d4154491acc108ab
MertAliEsen <81825582+MertAliEsen@users.noreply.github.com>
2026-06-18 16:52:44 +0200

    Add files via upload

---
+++ mySeventhApp.java
@@ -0,0 +1,18 @@
+public class mySeventhApp {
+    public static void main(String[] args) {
+        Person adam = new Person("Adam", "Nowak", 20, "Warsaw", 2006);
+
+        System.out.println("original data");
+        adam.displayInfo();
+
+        System.out.println();
+        adam.growOld(10);
+        System.out.println("after growOld 10");
+        adam.displayInfo();
+
+        System.out.println();
+        adam.beYounger();
+        System.out.println("after beYounger");
+        adam.displayInfo();
+    }
+}
