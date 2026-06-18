302409882324ee57703210335ea68a210715f0b3
MertAliEsen <81825582+MertAliEsen@users.noreply.github.com>
2026-06-18 16:49:39 +0200

    Add files via upload

---
+++ mySecondApp.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class mySecondApp {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int x = 10;
+        int twice = x * 2;
+        int squared = x * x;
+
+        System.out.println("x = " + x);
+        System.out.println("double of x = " + twice);
+        System.out.println("x squared = " + squared);
+
+        System.out.println("enter your age in years");
+        int age = input.nextInt();
+
+        long seconds = (long) age * 365 * 24 * 60 * 60;
+        System.out.println("my age in seconds " + seconds);
+
+        input.close();
+    }
+}
